9963d52f036c61e8fafcf14bd7f4b45a2fe644ca
kevin <hk850522@gmail.com>
2017-10-10 00:14:06 +0800

    Add Ray.cpp and Ray.h
    Add Ray's Class and it's attribute and method

--- RenderResearchProject/Ray.cpp
+++ RenderResearchProject/Ray.cpp
@@ -1,2 +1,15 @@
 #include "Ray.h"
 
+Ray::Ray()
+{
+	_source = vec3(0);
+	_dir = vec3(0);
+	_inverse = vec3(0);
+}
+
+vec3 Ray::getInverseDirection()
+{
+	for (int i = 0; i < 3; i++) {
+		_inverse[i] = (_dir[i] != 0.0 ? 1 / _dir[i] : 0);
+	}
+}
--- RenderResearchProject/Ray.h
+++ RenderResearchProject/Ray.h
@@ -1,9 +1,19 @@
 #pragma once
+#include "algebra3.h"
 
 class Ray
 {
 	public :
-
+		Ray();
+		Ray(const vec3 & source, const vec3 & dir) : _source(source), _dir(dir) 
+		{
+			_inverse = getInverseDirection();
+		};
 	private :
+		vec3 _source;
+		vec3 _dir; // direction
+		vec3 _inverse; // invserse fo _dir
+
+		vec3 getInverseDirection();
 
 };
